19059c77401b706dc9eb190bfb57944dfa499653
ArditZubaku <zubaku92@gmail.com>
2025-11-13 13:46:23 +0100

    Multiple type params

---
+++ cmd/print/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	PrintThings(1, 2, 3, 4)
+	PrintThings(1, 2.2, "3", 4)
+}
---
+++ cmd/print/print.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func PrintThings[A, B any, C ~int](a1, a2 A, b B, c C) {
+	fmt.Printf("%v %v %v %v\n", a1, a2, b, c)
+}
